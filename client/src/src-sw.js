941ade04b145b185e6c90e46c9980ce5a59323f1
WajhR <w_riyasat@hotmail.com>
2023-05-08 23:52:57 -0400

    Updated webpack.config.js

--- client/src/src-sw.js
+++ client/src/src-sw.js
@@ -9,7 +9,24 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 // The precacheAndRoute() method takes an array of URLs to precache. The self._WB_MANIFEST is an array that contains the list of URLs to precache.
 precacheAndRoute(self.__WB_MANIFEST);
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
 
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache)
 // Set up asset cache
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
